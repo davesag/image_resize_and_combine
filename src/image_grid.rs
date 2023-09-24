1c65339ae2220561e94946997911827da4a0641e
Dave Sag <davesag@gmail.com>
2023-09-24 14:59:24 +1000

    export functions

--- src/image_grid.rs
+++ src/image_grid.rs
@@ -1,3 +1,7 @@
 pub mod compare_images;
 pub mod create_image_grid;
 pub mod resize_image;
+
+pub use compare_images::compare_images;
+pub use create_image_grid::create_image_grid;
+pub use resize_image::resize_image;
